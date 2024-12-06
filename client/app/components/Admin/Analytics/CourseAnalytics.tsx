cbd19b1fe05f1e8bfaf7812a9b15ca7eb9c799d6
Abel mekonn <abelmekonn9@gmail.com>
2024-12-06 23:09:28 +0300

    commit

--- client/app/components/Admin/Analytics/CourseAnalytics.tsx
+++ client/app/components/Admin/Analytics/CourseAnalytics.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {BarChart,Bar,ResponsiveContainer,XAxis,Label,LabelList} from "recharts"
+import Loader from '../../loader'
 type Props = {}
 
 const CourseAnalytics = (props: Props) => {
   return (
-    <div>CourseAnalytics</div>
+    <div>
+      
+    </div>
   )
 }
 
