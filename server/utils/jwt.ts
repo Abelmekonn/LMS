f4f9a9f3cc2418a7eefb49cd252c8982e6992ded
Abel mekonn <abelmekonn9@gmail.com>
2025-02-18 22:08:39 +0300

    `Update JWT options to use sameSite="none" and secure=true`

--- server/utils/jwt.ts
+++ server/utils/jwt.ts
@@ -20,16 +20,16 @@ export const accessTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + 15 * 60 * 1000), // 15 minutes
     maxAge: 15 * 60 * 1000, // 15 minutes
     httpOnly: true,
-    sameSite: "lax",
-    secure: false, // Ensure this is false in non-HTTPS environments
+    sameSite: "none",
+    secure: true, // Ensure this is false in non-HTTPS environments
 };
 
 export const refreshTokenOptions: ITokenOptions = {
     expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7 days
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     httpOnly: true,
-    sameSite: "lax",
-    secure: false, // Ensure this is false in non-HTTPS environments
+    sameSite: "none",
+    secure: true, // Ensure this is false in non-HTTPS environments
 };
 
 export const sendToken = (user: IUser, statusCode: number, res: Response) => {
